d246e859ffa674976ec3d057212a99d79e7c792f
Aidan Dunne <aidandunne@Aidans-MacBook-Air.local>
2019-03-29 14:10:52 +0000

    volley added

--- app/src/main/java/com/example/premierleagueapp4/MainActivity.java
+++ app/src/main/java/com/example/premierleagueapp4/MainActivity.java
@@ -6,16 +6,30 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.google.gson.*;
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import android.util.Log;
+
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.*;
+
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    private String url ="https://applicationbackend.azurewebsites.net/api/";
+    private String TAG ="premierleagueinfo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +56,9 @@ public class MainActivity extends AppCompatActivity {
 
                 String test = whatever.getItem(position).toString();
 
-                Toast.makeText(getApplicationContext(), test, Toast.LENGTH_SHORT).show();
+                premierLeagueCall(view, test);
+
+                //Toast.makeText(getApplicationContext(), test, Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -50,4 +66,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void premierLeagueCall(View v, String team){
+        final TextView onClickText = findViewById(R.id.textView4);
+
+        try
+        {
+            // make a string request (JSON request an alternative)
+            RequestQueue queue = Volley.newRequestQueue(this);
+            Log.d(TAG, "Making request");
+            try
+            {
+                StringRequest strObjRequest = new StringRequest(Request.Method.GET, url+"club/"+team,
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response)
+                            {
+                                // parse resulting string containing JSON to Greeting object
+                                premierLeagueClass greeting = new Gson().fromJson(response, premierLeagueClass.class);
+                                 onClickText.setText(greeting.toString());
+                                Log.d(TAG, "Displaying data" + greeting.toString());
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error)
+                            {
+                                onClickText.setText(error.toString());
+                                Log.d(TAG, "Error" + error.toString());
+                            }
+                        });
+                queue.add(strObjRequest);           // can have multiple in a queue, and can cancel
+            }
+            catch (Exception e1)
+            {
+                Log.d(TAG, e1.toString());
+                onClickText.setText(e1.toString());
+            }
+        }
+        catch (Exception e2)
+        {
+            Log.d(TAG, e2.toString());
+            onClickText.setText(e2.toString());
+        }
+    }
+
+
 }
+
+
+
---
+++ app/src/main/java/com/example/premierleagueapp4/premierLeagueClass.java
@@ -0,0 +1,25 @@
+package com.example.premierleagueapp4;
+
+public class premierLeagueClass {
+    private String teamName;
+    private int clubValue;
+    private String homeKitColour;
+    private String awayKitColour;
+    private String thirdKitColour;
+    private int premierLeague;
+    private int championsLeague;
+    private int faCup;
+    private int europaLeague;
+    private int leagueCup;
+
+
+    public String toString()
+    {
+        return "Team Name: "+teamName + "\n"
+                +"Home Kit: "+ homeKitColour+"\n"
+                +"Away Kit: "+ awayKitColour+"\n"
+                +"Third Kit: "+thirdKitColour+"\n"
+                +"Club Value: "+ clubValue+"\n";
+    }
+
+}
